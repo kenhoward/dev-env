51cbbd43a106a839d72b0e61b5c6e02cb57dcd64
Kenny <ken.chad.howard@gmail.com>
2017-06-14 16:39:04 -0600

    finalizing mocha updates, preparing for continuous integration

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,26 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
+
+// Just Some Examples
 
 describe('First Test', () => {
     it('should pass', () => {
         expect(true).to.equal(true);
+    });
+});
+
+describe('index.html', () => {
+    it('should say hello', (done) => {
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        // can pass in an array of object
+        // unless fetch would need isomorphic functionality
+        jsdom.env(index, function(err, window) {
+            const h1 = window.document.getElementsByTagName('h1')[0];
+            // just first one
+            expect(h1.innerHTML).to.equal('Connected');
+            done();
+            window.close();
+        });
     });
 });
